c97592c35a3a412b78e57c95605b285110fdfbe2
bulsu-emagustin <2024100668@ms.bulsu.edu.ph>
2026-04-04 23:47:07 +0800

    Student Table and Bins Dialog

--- EDP/src/EDSxADS_Final_Project/RecyclingCollectionMonitoringSystem.java
+++ EDP/src/EDSxADS_Final_Project/RecyclingCollectionMonitoringSystem.java
@@ -29,7 +29,7 @@ public class RecyclingCollectionMonitoringSystem {
 
     //Main Method
     public static void main(String[] args) {
-        new UniversityRecycleZone();
+        new AdminFrame();
     }
 }
 
